31d23282444f6d6c3d8c962d0dd461123658c8e0
Nixos <bustion047@gmail.com>
2026-03-04 14:13:18 -0400

    Added migrations

---
+++ docker/ckan/docker-entrypoint.d/00_apply_pending_migrations.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+response="script: apply alembic migrations for each plugin;"
+
+if [[ -z "$CKAN_APPLY_ALEMBIC_MIGRATIONS" ]]; then
+  echo "Attempt to perform alembic migrations for every plugin"
+  ckan -c $CKAN_INI db pending_migrations --apply
+  if [[ $? -ne 0 ]]; then
+    response="$response cmd: ckan -c $CKAN_INI db pending_migrations --apply: non-zero exit, check logs manually;"
+  else
+    response="$response ckan alembic migrations completed;"
+  fi
+else
+  response="$response CKAN_APPLY_ALEMBIC_MIGRATIONS env variable not set, not performing migrations."
+fi
+
+echo $response
